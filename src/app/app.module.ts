6fa136317018a88d97ecc944437cdb49d9a61edd
Kestas <kestasnav@gmail.com>
2022-10-18 13:34:42 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { RezervuarasComponent } from './components/rezervuaras/rezervuaras.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RezervuarasComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
